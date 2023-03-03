413c667d5bec6f1741454953ccb1ff6c923bbd4d
Kenji Mobutu <kenjimobutu@gmail.com>
2023-03-03 14:27:12 +0100

    refactor(model User)

--- prbd-2223-a14/Model/User.cs
+++ prbd-2223-a14/Model/User.cs
@@ -5,4 +5,7 @@ namespace MyPoll.Model;
 public class User : EntityBase<MyPollContext> {
     public int UserId { get; set; }
     public string Name { get; set; }
+    public string Mail { get; set; }
+    public string Password { get; set; }
+    public Boolean IsAdmin { get; set; }
 }
