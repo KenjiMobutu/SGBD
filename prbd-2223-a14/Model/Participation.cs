98fa3f81dae8ac353d5dcab5e1de437a975ed00e
Kenji Mobutu <kenjimobutu@gmail.com>
2023-04-01 16:56:31 +0200

    feat(Model) adding Participation table

---
+++ prbd-2223-a14/Model/Participation.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using PRBD_Framework;
+
+namespace MyPoll.Model;
+
+public class Participation : EntityBase<MyPollContext> {
+    [ForeignKey(nameof(Poll))]
+    public int PollId { get; set; }
+    public virtual Poll Poll { get; set; }
+
+
+    [ForeignKey(nameof(User))]
+    public int UserId { get; set; }
+    public virtual User User { get; set; }
+    //public virtual ICollection<User> Participate { get; set; } = new HashSet<User>();
+    //public virtual ICollection<Poll> FollowedBy { get; set; } = new HashSet<Poll>();
+
+}
