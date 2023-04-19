fb463a2d1f98bd431e17fabbb8161187f7862246
Kenji Mobutu <kenjimobutu@gmail.com>
2023-04-19 11:42:03 +0200

    feat(View +VM): Polls first view

--- prbd-2223-a14/ViewModel/LoginViewModel.cs
+++ prbd-2223-a14/ViewModel/LoginViewModel.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows.Input;
 using MyPoll.Model;
@@ -34,25 +35,39 @@ public class LoginViewModel : ViewModelCommon{
             NotifyColleagues(App.Messages.MSG_LOGIN, user);
         }
     }
-    public override bool Validate() {
-        ClearErrors();
-        var user = Context.Users.SingleOrDefault(user => user.Mail == Mail);
 
+    public bool ValidateEmail() {
+        var user = Context.Users.SingleOrDefault(user => user.Mail == Mail);
+        Regex regex = new Regex(@"^([\w.-]+)@([\w-]+)((.(\w){2,3})+)$");
+        Match match = regex.Match(Mail);
         if (string.IsNullOrEmpty(Mail))
             AddError(nameof(Mail), "required");
         else if (Mail.Length < 3)
             AddError(nameof(Mail), "length must be >= 3");
+        else if (!match.Success)
+            AddError(nameof(Mail), "Incorrect Mail");
         else if (user == null)
             AddError(nameof(Mail), "does not exist");
-        else {
-            if (string.IsNullOrEmpty(Password))
-                AddError(nameof(Password), "required");
-            else if (Password.Length < 3)
-                AddError(nameof(Password), "length must be >= 3");
-            else if (user != null && user.Password != Password)
-                AddError(nameof(Password), "wrong password");
-        }
+        
+        return !HasErrors;
+    }
 
+    public bool ValidatePassword() {
+        var user = Context.Users.FirstOrDefault(user => user.Mail == Mail);
+
+        if (string.IsNullOrEmpty(Password))
+            AddError(nameof(Password), "required");
+        else if (Password.Length < 3)
+            AddError(nameof(Password), "length must be >= 3");
+        else if (user != null && !SecretHasher.Verify(Password, user.Password))
+            AddError(nameof(Password), "wrong password");
+
+        return !HasErrors;
+    }
+    public override bool Validate() {
+        ClearErrors();
+        ValidateEmail();
+        ValidatePassword();
         return !HasErrors;
     }
     protected override void OnRefreshData() {
