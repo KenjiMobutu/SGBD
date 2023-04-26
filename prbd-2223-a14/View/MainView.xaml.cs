bf36ae0013a8d458d490134f623e4b3f6c2b7857
Kenji Mobutu <kenjimobutu@gmail.com>
2023-04-26 22:51:21 +0200

    feat: LogOut + onclosing

--- prbd-2223-a14/View/MainView.xaml.cs
+++ prbd-2223-a14/View/MainView.xaml.cs
@@ -1,10 +1,23 @@
 ﻿using PRBD_Framework;
 using MyPoll.Model;
+using System.Windows.Input;
+using System.ComponentModel;
 
 namespace MyPoll.View;
 
 public partial class MainView : WindowBase {
     public MainView() {
         InitializeComponent();
+    }
+    private void MenuLogout_Click(object sender, System.Windows.RoutedEventArgs e) {
+        NotifyColleagues(App.Messages.MSG_LOGOUT);
+    }
+    private void WindowBase_KeyDown(object sender, KeyEventArgs e) {
+        if (e.Key == Key.Q && Keyboard.IsKeyDown(Key.LeftCtrl))
+            Close();
+    }
+    protected override void OnClosing(CancelEventArgs e) {
+        base.OnClosing(e);
+        tabControl.Dispose();
     }
 }
