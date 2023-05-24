b45405c27e218aa8c4e5b0d10ae2a1d350090407
Kenji Mobutu <kenjimobutu@gmail.com>
2023-05-24 15:55:49 +0200

    Feat: Add converter to cut long text (Commens, Choices)

---
+++ prbd-2223-a14/ViewModel/TruncateTextConverter.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+using System;
+
+using PRBD_Framework;
+
+
+namespace MyPoll.ViewModel;
+
+public class TruncateTextConverter : IValueConverter {
+    public object Convert(object value, Type targetType, object parameter, CultureInfo culture) {
+        if (value is string text && parameter is string lengthString && int.TryParse(lengthString, out int maxLength)) {
+            if (text.Length > maxLength) {
+                return text.Substring(0, maxLength) + "...";
+            } else {
+                return text;
+            }
+        }
+        return value;
+    }
+
+    public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture) {
+        throw new NotSupportedException();
+    }
+}
